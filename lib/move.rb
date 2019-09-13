38d89c83b4b600ff15955d710c5a26242c14fcbf
Jazmin M <jazminmerckel@gmail.com>
2019-09-13 18:32:56 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,36 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+# This is what our board will look like when we 
+# are playing the game. It holds the current 
+# state of the tic tac toe board.
+board_array = [
+  " ", " ", " ",
+  " ", " ", " ",
+  " ", " ", " "
+]
+
+# In this example, the user typed the number 
+# '3' and we converted it into the integer 2.
+index = 2
+
+# At the start of the game, X always goes first
+# character is only going to be either "X" or "O"
+character = "X"
+
+# This move method represents one move in the
+# tic tac toe game. To perform a move, we 
+# will take the current board_array and 
+# modify it based on where the user wants to
+# move.
+def move(board_array, index, character)
+  board_array[index] = character
+end
+  
+  
+  
